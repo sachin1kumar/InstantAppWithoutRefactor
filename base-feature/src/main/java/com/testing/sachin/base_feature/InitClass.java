ec75f0096fcdec4052dbbff645d75efe092e5576
sachin1kumar <sachinkmr375@gmail.com>
2017-09-10 10:52:15 +0530

    my first commit

--- base-feature/src/main/java/com/testing/sachin/base_feature/InitClass.java
+++ base-feature/src/main/java/com/testing/sachin/base_feature/InitClass.java
@@ -1,8 +1,14 @@
 package com.testing.sachin.base_feature;
 
+import android.util.Log;
+
 /**
  * Created by sachin on 10/9/17.
  */
 
 public class InitClass {
+
+    public void print(){
+        Log.e("InitClass:","base-feature:print");
+    }
 }
--- first-feature/src/main/java/com/testing/sachin/first_feature/SecondActivity.java
+++ first-feature/src/main/java/com/testing/sachin/first_feature/SecondActivity.java
@@ -3,11 +3,16 @@ package com.testing.sachin.first_feature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.testing.sachin.base_feature.InitClass;
+
 public class SecondActivity extends AppCompatActivity {
 
+    private InitClass initClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        initClass = new InitClass();
+        initClass.print();
     }
 }
